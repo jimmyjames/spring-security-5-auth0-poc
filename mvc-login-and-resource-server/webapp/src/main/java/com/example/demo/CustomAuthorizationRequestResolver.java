4ff837ad5d09ab4599d1fe3ad830de33799e672e
Jim Anderson <jim.anderson@auth0.com>
2019-06-11 08:30:41 -0500

    Don't need to set prompt param on auth request

--- mvc-login-and-resource-server/webapp/src/main/java/com/example/demo/CustomAuthorizationRequestResolver.java
+++ mvc-login-and-resource-server/webapp/src/main/java/com/example/demo/CustomAuthorizationRequestResolver.java
@@ -1,14 +1,10 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedHashMap;
@@ -45,7 +41,6 @@ public class CustomAuthorizationRequestResolver implements OAuth2AuthorizationRe
     private OAuth2AuthorizationRequest customAuthorizationRequest(OAuth2AuthorizationRequest request) {
         LinkedHashMap<String, Object> params = new LinkedHashMap<>(request.getAdditionalParameters());
         params.put("audience", audience);
-        params.put("prompt", "consent");
 
         OAuth2AuthorizationRequest appendedRequest =
                 OAuth2AuthorizationRequest.from(request).additionalParameters(params).build();
